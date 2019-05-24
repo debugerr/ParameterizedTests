91caf00ff5a98e620e2d9729372ed979b86a0d5f
Patrick Schuler <paschule@microsoft.com>
2019-05-24 14:43:19 +0200

    sample tests

---
+++ PassingParams.Test/UnitTest1.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+using System;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [Category("cat1")]
+        public void Test1()
+        {
+            Assert.Pass();
+            Console.WriteLine("Test1");
+        }
+
+        [Test]
+        [Category("cat2")]
+        public void Test2()
+        {
+            Assert.Pass();
+            Console.WriteLine("Test2");
+        }
+    }
+}
